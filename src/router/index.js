45509ac8186861eab39b0964d9bc3c96bfd354fd
Keyliyh <920587050@qq.com>
2020-03-30 14:04:58 +0800

    //去掉APP多余信息

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/about/index.vue")
-  }
+  },
+  //登录
+  {
+    path: "/login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/login/index.vue")
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: liyh
+ * @Date: 2020-03-30 14:02:41
+ * @LastEditors: liyh
+ * @LastEditTime: 2020-03-30 14:04:09
+ -->
+<template>
+  <div class="home">登录</div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Login",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
