0a12a63f2fecda31b09a8fea7f430f2a7e74158d
Keyliyh <920587050@qq.com>
2020-03-30 18:47:29 +0800

    //登录注册85%

---
+++ src/views/login/components/register.vue
@@ -0,0 +1,282 @@
+<!--
+ * @Author: liyh
+ * @Date: 2020-03-30 16:13:50
+ * @LastEditors: liyh
+ * @LastEditTime: 2020-03-30 18:44:04
+ -->
+<template>
+  <!-- 注册部分 -->
+  <div>
+    <div class="userTypeBox">
+      <div
+        :class="['userType',userType==='personal'? 'active':'default']"
+        @click="changeUesrType('personal')"
+      >
+        <span>个人用户</span>
+      </div>
+      <div
+        :class="['userType',userType==='company'? 'active':'default']"
+        @click="changeUesrType('company')"
+      >
+        <span>企业用户</span>
+      </div>
+    </div>
+
+    <div>
+      <div v-if="userType==='personal'">
+        <div class="inputBox">
+          <div class="left">用户名</div>
+          <div class="inputArea">
+            <input type="text" v-model="userName" maxlength="11" placeholder="请输入5~20位的用户名" />
+          </div>
+        </div>
+        <div class="inputBox">
+          <div class="left">手机号</div>
+          <div class="inputArea">
+            <input
+              @focus="resetErrortext('telephone')"
+              v-model="telephone"
+              type="text"
+              maxlength="11"
+              placeholder="请输入手机号"
+            />
+          </div>
+        </div>
+        <div class="inputBox">
+          <div class="left">验证码</div>
+          <div class="inputArea">
+            <input
+              v-model="qrcode"
+              @focus="resetErrortext('qrcode')"
+              maxlength="6"
+              type="text"
+              placeholder="请输入验证码"
+            />
+          </div>
+          <div :class="[enAbleStatus?'qrEnable':'qrDisAble','right']" @click="getQrcode">
+            <span>{{qrText}}</span>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <div class="inputBox">
+          <div class="left">企业名称</div>
+          <div class="inputArea">
+            <input
+              v-model="companyName"
+              @focus="resetErrortext('companyName')"
+              type="text"
+              placeholder="请输入企业名称"
+            />
+          </div>
+        </div>
+        <div class="inputBox">
+          <div class="left">企业邮箱</div>
+          <div class="inputArea">
+            <input
+              v-model="companyEmail"
+              @focus="resetErrortext('companyEmail')"
+              type="text"
+              placeholder="请输入企业邮箱"
+            />
+          </div>
+        </div>
+        <div class="inputBox">
+          <div class="left">设置密码</div>
+          <div class="inputArea">
+            <input
+              v-model="setPassword"
+              @focus="resetErrortext('setPassword')"
+              type="text"
+              maxlength="10"
+              placeholder="请设置6-10位的密码"
+            />
+          </div>
+        </div>
+        <div class="inputBox">
+          <div class="left">确认密码</div>
+          <div class="inputArea">
+            <input
+              v-model="confirmPassword"
+              @focus="resetErrortext('confirmPassword')"
+              type="text"
+              maxlength="10"
+              placeholder="请再次输入密码"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="errorTips">{{errorMassage}}</div>
+    <div @click="toRegister" class="btnBox">
+      <span class="defaultText">注册</span>
+    </div>
+  </div>
+</template>
+
+<script>
+const telephoneReg = /^1[3456789]\d{9}$/;
+export default {
+  name: "Login",
+  data() {
+    return {
+      userType: "personal", //用户类型:personal=>个人用户;company=>企业用户
+      errorType: "",
+      errorMassage: "",
+      qrText: "获取验证码",
+      enAbleStatus: true,
+      qrTimer: null,
+      userName: "", //用户名
+      telephone: "", //用户手机号
+      qrcode: "", //验证码
+      companyName: "", //企业名称
+      companyEmail: "", //企业邮箱
+      setPassword: "", //设置邮箱
+      confirmPassword: "" //确认密码
+    };
+  },
+  methods: {
+    /**
+     * @description: 切换用户类型。个人用户/企业用户
+     */
+    changeUesrType(type) {
+      this.userType = type;
+      this.errorMassage = "";
+    },
+
+    /**
+     * @description: 重置错误提示
+     */
+    resetErrortext(type) {
+      if (this.errorType == type) {
+        this.errorMassage = "";
+      }
+    },
+
+    /**
+     * @description: 校验字段是否合法
+     */
+    verify(type) {
+      const {
+        userName,
+        telephone,
+        qrcode,
+        companyName,
+        companyEmail,
+        setPassword,
+        confirmPassword
+      } = this;
+      switch (type) {
+        case "personal":
+          if (!userName) {
+            this.errorType = "userName";
+            this.errorMassage = "请输入用户名";
+            return false;
+          } else if (!(userName.length >= 4 && userName.length <= 20)) {
+            this.errorType = "userName";
+            this.errorMassage = "用户名错误，请重新输入";
+            return false;
+          } else if (!telephone) {
+            this.errorType = "telephone";
+            this.errorMassage = "请输入手机号";
+            return false;
+          } else if (!telephoneReg.test(telephone)) {
+            this.errorType = "telephone";
+            this.errorMassage = "手机号错误，请重新输入";
+            return false;
+          } else if (!qrcode) {
+            this.errorType = "qrcode";
+            this.errorMassage = "请输入验证码";
+            return false;
+          } else if (qrcode.length != 6) {
+            this.errorType = "qrcode";
+            this.errorMassage = "验证码错误，请重新输入";
+            return false;
+          }
+          return true;
+          break;
+        case "company":
+          if (!companyName) {
+            this.errorType = "companyName";
+            this.errorMassage = "请输入企业名称";
+            return false;
+          } else if (!companyEmail) {
+            this.errorType = "companyEmail";
+            this.errorMassage = "请输入企业邮箱";
+            return false;
+          } else if (!setPassword) {
+            this.errorType = "setPassword";
+            this.errorMassage = "请输入设置密码";
+            return false;
+          } else if (!(setPassword.length >= 6 && setPassword.length <= 10)) {
+            this.errorType = "confirmPassword";
+            this.errorMassage = "请设置6-10位的密码";
+            return false;
+          } else if (!confirmPassword) {
+            this.errorType = "confirmPassword";
+            this.errorMassage = "请再次输入密码";
+            return false;
+          } else if (confirmPassword != setPassword) {
+            this.errorType = "confirmPassword";
+            this.errorMassage = "两次输入的密码不一致，请重新输入";
+            return false;
+          }
+          return true;
+          break;
+        default:
+          break;
+      }
+    },
+
+    /**
+     * @description: 点击获取验证码按钮
+     */
+    getQrcode() {
+      const { userName, telephone } = this;
+      if (!userName) {
+        this.errorType = "userName";
+        this.errorMassage = "请输入用户名";
+        return false;
+      } else if (!(userName.length >= 4 && userName.length <= 20)) {
+        this.errorType = "userName";
+        this.errorMassage = "用户名错误，请重新输入";
+        return false;
+      } else if (!telephone) {
+        this.errorType = "telephone";
+        this.errorMassage = "请输入手机号";
+        return false;
+      } else if (!telephoneReg.test(telephone)) {
+        this.errorType = "telephone";
+        this.errorMassage = "手机号错误，请重新输入";
+        return false;
+      }
+      let second = 20;
+      if (this.qrTimer) return false;
+      this.qrTimer = setInterval(() => {
+        second -= 1;
+        if (this.enAbleStatus) {
+          this.enAbleStatus = false;
+        }
+        if (second >= 0) {
+          this.qrText = `已发送(${second}s)`;
+        } else {
+          this.enAbleStatus = true;
+          clearInterval(this.qrTimer);
+          this.qrTimer = null;
+          this.qrText = "重新获取";
+        }
+      }, 1000);
+    },
+
+    /**
+     * @description: 点击登录按钮
+     */
+    toRegister() {
+      this.verify(this.userType); //先校检
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,29 +2,73 @@
  * @Author: liyh
  * @Date: 2020-03-30 14:02:41
  * @LastEditors: liyh
- * @LastEditTime: 2020-03-30 14:10:41
+ * @LastEditTime: 2020-03-30 18:10:39
  -->
 <template>
   <div class="Wrapper">
     <div class="bg"></div>
-    <div>登录</div>
+    <div class="box">
+      <div class="operationBox">
+        <div>返回</div>
+        <div @click="changeOperationType">{{operationType==0?'去注册':'返回登录'}}</div>
+      </div>
+      <!-- 登录部分 -->
+      <div v-if="operationType==0">
+        <LoginPart></LoginPart>
+      </div>
+      <!-- 注册部分 -->
+      <div v-else>
+        <RegisterPart></RegisterPart>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+import LoginPart from "./components/login.vue";
+import RegisterPart from "./components/register.vue";
 
 export default {
   name: "Login",
   components: {
-    // HelloWorld
+    LoginPart,
+    RegisterPart
+  },
+  data() {
+    return {
+      operationType: 0 //0代表登录，1代表注册
+    };
+  },
+  methods: {
+    changeUesrType(type) {
+      this.userType = type;
+    },
+    changeOperationType() {
+      this.operationType = ~this.operationType + 2; //0变1，1变0
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
+#app {
+  height: 100vh;
+}
 .Wrapper {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  position: relative;
+  top: 12%;
+  font-weight: 400;
   .bg {
     position: fixed;
     top: 0;
@@ -33,6 +77,136 @@ export default {
     bottom: 0;
     background: url("./../../assets/login/bg@2x.png") no-repeat;
     background-size: 100% 100%;
+    z-index: -1;
+  }
+  .box {
+    width: 460px;
+    padding: 56px 60px 64px 60px;
+    box-sizing: border-box;
+    background: #ffffff;
+    position: relative;
+    .operationBox {
+      padding: 0 2px;
+      position: absolute;
+      top: -28px;
+      left: 0;
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      color: #ffffff;
+      font-size: 14px;
+      & div:last-child {
+        text-decoration: underline;
+      }
+    }
+    .userTypeBox {
+      display: flex;
+      width: 100%;
+      font-size: 20px;
+      font-weight: 400;
+      margin-bottom: 20px;
+      .userType {
+        flex: 1;
+        position: relative;
+        padding-bottom: 10px;
+      }
+      .active {
+        color: #1dbc76;
+      }
+      .default {
+        color: #000000;
+        span {
+          opacity: 0.4;
+        }
+      }
+      & .active::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        width: 170px;
+        height: 1px;
+        left: 0;
+        background: #1dbc76;
+      }
+      & .default::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        width: 170px;
+        height: 1px;
+        left: 0;
+        background: #000000;
+        opacity: 0.06;
+      }
+    }
+    .inputBox {
+      width: 340px;
+      height: 50px;
+      border: 1px solid rgba(0, 0, 0, 0.2);
+      margin-top: 20px;
+      display: flex;
+      padding: 0px 16px;
+      align-items: center;
+      .left {
+        font-size: 16px;
+        font-weight: 400;
+        color: #000000;
+        margin-right: 20px;
+        opacity: 0.8;
+      }
+      .inputArea {
+        flex: 1;
+        height: 100%;
+        input {
+          width: 100%;
+          height: 100%;
+          font-size: 16px;
+          color: #000000;
+          font-weight: 400;
+        }
+      }
+      .right {
+        width: 90px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        background: #f0f0f0;
+        border-radius: 2px;
+        color: #000000;
+        font-size: 14px;
+        font-weight: 400;
+      }
+      .qrEnable {
+        color: #1dbc76;
+      }
+      .qrDisAble {
+        span {
+          opacity: 0.2;
+        }
+      }
+    }
+    .errorTips {
+      text-align: left;
+      margin-top: 8px;
+      color: #de4949;
+      font-size: 14px;
+    }
+    .btnBox {
+      width: 340px;
+      height: 56px;
+      background: rgba(29, 188, 118, 1);
+      font-size: 22px;
+      font-weight: 400;
+      margin-top: 50px;
+      line-height: 56px;
+      &:hover .defaultText {
+        opacity: 1;
+      }
+      .defaultText {
+        color: white;
+        opacity: 0.4;
+      }
+    }
   }
 }
 </style>
