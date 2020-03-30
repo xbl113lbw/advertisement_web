27fefc66f28f80f0f14f90621b1986b0d4800bab
Keyliyh <920587050@qq.com>
2020-03-30 11:20:31 +0800

    //项目结构

---
+++ src/views/about/index.vue
@@ -0,0 +1,11 @@
+<!--
+ * @Author: liyh
+ * @Date: 2020-03-30 11:05:25
+ * @LastEditors: liyh
+ * @LastEditTime: 2020-03-30 11:05:25
+ -->
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,12 @@
+<!--
+ * @Author: liyh
+ * @Date: 2020-03-30 11:04:57
+ * @LastEditors: liyh
+ * @LastEditTime: 2020-03-30 11:06:30
+ -->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="./../../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
